2c5e8d6cef111904aa954863e4c34b7ed6d70314
Fawazino007 <fawazalhubail@gmail.com>
2026-04-15 16:47:48 +0300

    fixed identity

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -1,5 +1,7 @@
 import MarketplaceBrowse from '@/components/MarketplaceBrowse';
 
+export const dynamic = 'force-dynamic';
+
 export default function MarketplacePage() {
   return <MarketplaceBrowse />;
 }
